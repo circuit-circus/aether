3fd703afd197640b2d2c2868115e3e62f954fdb5
Nina Cecilie Højholdt <nhoejholdt@gmail.com>
2018-02-15 23:30:55 +0100

    Start working on the GUI

--- server_websocket/public/js/src/main.js
+++ server_websocket/public/js/src/main.js
@@ -1,5 +1,87 @@
+var planets = [];
+
+/*
+    States:
+    1 = Title screen
+    2 = Input question
+    3 = Pick planet
+    4 = Transmit and recieve
+*/
+var programState = 1;
+
+
+function setup() {
+    var myCanvas = createCanvas(1000, 400);
+    myCanvas.parent('planet-container');
+
+    fill(0);
+    stroke(255);
+
+    for(var i = 0; i < 8; i++) {
+        var diameter = random(70, 110);
+        planets[i] = new Planet(i * 125 + ((125-diameter) / 2 + diameter/2), 200, diameter);
+    }
+}
+
+function draw() {
+    for(var i = 0; i < 8; i++) {
+        planets[i].display();
+    }
+}
+
+function Planet(xPos, yPos, dia) {
+    this.x = xPos;
+    this.y = yPos;
+    this.diameter = dia;
+
+    this.display = function() {
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
+}
+
 $(document).ready(function() {
 
+    $('button').on('click', function() {
+        $('#planet-container').toggleClass('active');
+    });
+
+    // Check for key inputs
+    $('body').on('keydown', function(e) {
+        console.log(e.which);
+
+        if(programState == 1) { // Title screen
+            e.preventDefault();
+            $('main').attr('data-state', 2);
+            $('#question-input-field').focus();
+            programState = 2;
+
+        } else if (programState == 2) { // Input question
+
+            // Check for accepted keys
+            if (([8, 16, 32, 37, 38, 39, 40, 186, 222, 219, 189].indexOf(e.which) > -1) || // backspace, shift, space, arrow keys, æøå, dash
+                (e.which >= 48 && e.which <= 57) || // numbers
+                (e.which >= 65 && e.which <= 90) ) { // letters
+
+                if(e.which == 38) { // up
+                    $('#question-starter-rotator').animate({top: '+=50px'});
+                    $('#question-starter-rotator .focus').removeClass('focus').prev().addClass('focus');
+                    /*
+                    if($('#question-starter-rotator .focus').is('#question-starter-rotator span:first')) {
+                        $('#question-starter-rotator span').last().insertBefore('#question-starter-rotator .focus');
+                    }*/
+                } else if (e.which == 40) { // down
+                    $('#question-starter-rotator').animate({top: '-=50px'});
+                    $('#question-starter-rotator .focus').removeClass('focus').next().addClass('focus');
+                }
+            } else {
+                e.preventDefault();
+                console.log('not one of those');
+            }
+        }
+    });
+
+
+    /*
     $('.arduino-led').on('click', function(e) {
         var target = $(this).attr('data-target');
 
@@ -16,6 +98,7 @@ $(document).ready(function() {
             }
         });
     });
+    */
 
 });
 
