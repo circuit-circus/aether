ce6d2f5394a191f6dc8e3738541c3a773c4a8c66
Nina Cecilie Højholdt <nhoejholdt@gmail.com>
2018-03-02 12:36:24 +0100

    Write all the arduino TUI code

--- arduino_tui/arduino_tui.ino
+++ arduino_tui/arduino_tui.ino
@@ -1,13 +1,35 @@
-int questionPotPin = A4;
-int planetPotPin = A5;
+const int questionPotPin = A4;
+int questionPotVal = 0;
+int questionVal;
+int lastQuestionVal = questionVal;
+const int NO_OF_PLANETS = 8;
 
-int backButton = 5;
-int forwardButton = 6;
-int backLED = 3;
-int forwardLED = 4;
+const int planetPotPin = A5;
+const int numReadingsPlanetPot = 10;
+int readingsPlanetPot[numReadingsPlanetPot];      // the readings from the analog input
+int readIndexPlanetPot = 0;              // the index of the current reading
+int totalPlanetPot = 0;                  // the running total
+int averagePlanetPot = 0;
+int planetVal;
+int lastPlanetVal = planetVal;
+const int NO_OF_QUESTIONS = 6;
 
-int activeLED = 0;
+const int backButtonPin = 2;
+int backButtonState;
+int lastBackButtonState;
 
+const int continueButtonPin = 3;
+int continueButtonState;
+int lastContinueButtonState;
+
+const int transmitButtonPin = 4;
+int transmitButtonState;
+int lastTransmitButtonState;
+
+const int transmitButtonLEDPin = 6;
+int transmitButtonLEDValue;
+long transmitButtonLEDTime=0;
+int transmitButtonLEDPeriode = 2000;
 
 void setup() {
   Serial.begin(9600);
@@ -15,51 +37,94 @@ void setup() {
 
   pinMode(questionPotPin, INPUT);
   pinMode(planetPotPin, INPUT);
-  
-  pinMode(backButton, INPUT_PULLUP);
-  pinMode(forwardButton, INPUT_PULLUP);
-  
-  pinMode(backLED, OUTPUT);
-  pinMode(forwardLED, OUTPUT);
 
-  digitalWrite(backLED, HIGH);
-  digitalWrite(forwardLED, HIGH);
+  pinMode(backButtonPin, INPUT_PULLUP);
+  pinMode(continueButtonPin, INPUT_PULLUP);
+  pinMode(transmitButtonPin, INPUT_PULLUP);
+
+  pinMode(transmitButtonLEDPin, OUTPUT);
+
+  // Initialize planet potentiometer average readings
+  for (int thisReadingPlanetPot = 0; thisReadingPlanetPot < numReadingsPlanetPot; thisReadingPlanetPot++) {
+    readingsPlanetPot[thisReadingPlanetPot] = 0;
+  }
 
   delay(2000);
   Serial.println("BEGIN!");
 
 }
 
-void loop() {
+void loop() {  
 
-  //int questionPotVal = analogRead(questionPotPin);
-  //int planetPotVal = analogRead(planetPotPin);
-
-  long questionPotVal = random(300);
-  long planetPotVal = random(300);
-
-  Serial.print("QUESTION_POT: ");
-  Serial.println(questionPotVal);
-  delay(random(10000));
-  Serial.print("PLANET_POT: ");
-  Serial.println(planetPotVal);
-  delay(random(10000));
-
-  // Send potentiometer vals to server
-  //Serial.println(questionPotVal + "," + planetPotVal);
-
-  // If a button is pressed, send that to server
-  /*
-  if(digitalRead(backButton)) {
-    // Back button Pressed
-    Serial.println("BACK");
+  /* QUESTION (WHEEL) POTENTIOMETER */
+  questionPotVal = analogRead(questionPotPin);
+  questionVal = map(questionPotVal, 0, 1023, 0, NO_OF_PLANETS-1);
+  if(questionVal != lastQuestionVal) {
+    Serial.print("QUESTION: ");
+    Serial.println(questionVal);
   }
-  else if(digitalRead(forwardButton)) {
-    // Forward button Pressed
-    Serial.println("FORWARD");
-  }
-  */
+  lastQuestionVal = questionVal;
 
+
+  /* PLANET SLIDER POTENTIOMETER */
+  averagePlanetPot = calculatePlanetPotAverage();
+  planetVal = map(averagePlanetPot, 219, 956, 0, NO_OF_QUESTIONS-1);
+  if(planetVal != lastPlanetVal) {
+    Serial.print("PLANET: ");
+    Serial.println(planetVal);
+  }
+  lastPlanetVal = planetVal;
+
+
+  /* BUTTONS */
+  backButtonState = digitalRead(backButtonPin);
+  if(backButtonState != lastBackButtonState) {
+    Serial.println("BUTTON: BACK");
+  }
+  lastBackButtonState = backButtonState;
+
+  continueButtonState = digitalRead(continueButtonPin);
+  if(continueButtonState != lastContinueButtonState) {
+    Serial.println("BUTTON: CONTINUE");
+  }
+  lastContinueButtonState = continueButtonState;
+
+  transmitButtonState = digitalRead(transmitButtonPin);
+  if(transmitButtonState != lastTransmitButtonState && transmitButtonState == LOW) {
+    Serial.println("BUTTON: TRANSMIT");
+    delay(10);
+  }
+  
+  lastTransmitButtonState = transmitButtonState;
+
+  /* TRANSMIT BUTTON LIGHT */
+ 
+  transmitButtonLEDTime = millis();
+  transmitButtonLEDValue = 128 + 127 * cos(2 * PI / transmitButtonLEDPeriode * transmitButtonLEDTime);
+  analogWrite(transmitButtonLEDPin, transmitButtonLEDValue);           // sets the value (range from 0 to 255)
+
+}
+
+int calculatePlanetPotAverage() {
+  // subtract the last reading:
+  totalPlanetPot = totalPlanetPot - readingsPlanetPot[readIndexPlanetPot];
+  // read from the sensor:
+  readingsPlanetPot[readIndexPlanetPot] = analogRead(planetPotPin);
+  // add the reading to the total:
+  totalPlanetPot = totalPlanetPot + readingsPlanetPot[readIndexPlanetPot];
+  // advance to the next position in the array:
+  readIndexPlanetPot++;
+
+  // if we're at the end of the array...
+  if (readIndexPlanetPot >= numReadingsPlanetPot) {
+    // ...wrap around to the beginning:
+    readIndexPlanetPot = 0;
+  }
+
+  // calculate the average:
+  return totalPlanetPot / numReadingsPlanetPot;
+  
+  delay(1);
 }
 
 
